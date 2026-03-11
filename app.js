e08c18faf9bfa9be117ba9513798552e96cccc29
nevetha-0102 <your-email@example.com>
2026-03-11 20:58:27 +0530

    day 1 - node.js modules mini project

---
+++ app.js
@@ -0,0 +1,9 @@
+const{add,multiply,subract} = require('./math');
+const{capitalize,reverse} = require('./string')
+
+console.log('addition:  ',add(1,2));
+console.log('multiplication:  ',multiply(2,5));
+console.log('subraction:  ',subract(23,8));
+
+console.log('capitalized:  ',capitalize('nevetha'));
+console.log('reversed:  ',reverse('nevetha'));
