462da4ebef5785b95c7b8f2a13c8cb5bac32093c
nevetha-0102 <your-email@example.com>
2026-03-13 20:30:06 +0530

    added day 3 file

---
+++ stringbasics.js
@@ -0,0 +1,9 @@
+let firstname = "nevetha"
+let lastname = "jayasundar"
+let age = "20"
+
+console.log(firstname.length)
+let fullname = firstname+""+lastname
+console.log(fullname)
+console.log(typeof firstname)
+console.log(typeof age)
---
+++ stringmethods.js
@@ -0,0 +1,15 @@
+let name = "nevetha jayasundar"
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.includes("vaishu"))
+console.log(name.includes("dhanu"))
+console.log(name.indexOf("dharshini"))
+console.log(name.indexOf("appa"))
+console.log(name.replace("appa","lordshiva"))
+let word ="      helloworld       "
+console.log(word.trim())
+let sentence = "i love you grandpa"
+console.log(sentence.split(""))
+let newname = "newscript"
+console.log(newname.slice(0,4))
+console.log(newname.slice(4))
